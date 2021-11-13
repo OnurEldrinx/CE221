ccc964a272c5608ab5253294371169f4024f0894
Onur Öztop <onureldrinx@gmail.com>
2021-11-13 23:34:48 +0300

    stack implementation by using singly linked list

---
+++ src/Stacks/LinkedListStack.java
@@ -0,0 +1,115 @@
+package Stacks;
+
+public class LinkedListStack<T> {
+
+    StackNode<T> top;
+
+    public boolean isEmpty(){
+
+        return (top == null);
+
+    }
+
+    public void push(T data){
+
+        StackNode<T> newData = new StackNode<>(data);
+
+        if (top == null){
+
+            top = newData;
+
+        }else{
+
+            StackNode<T> temp = top;
+            top = newData;
+            newData.next = temp;
+
+        }
+
+
+    }
+
+    public void pop(){
+
+        if (isEmpty()){
+
+            System.out.println("Stack is empty.");
+
+        }
+
+        top = top.next;
+
+    }
+
+    public T peek(){
+
+        if (isEmpty()){
+
+            System.out.println("Stack is empty.");
+
+        }
+
+        return top.data;
+
+
+    }
+
+    public T topAndPop(){
+
+        if (isEmpty()){
+
+            System.out.println("Stack is empty.");
+
+        }
+
+        T p = top.data;
+        top = top.next;
+
+
+        return p;
+
+    }
+
+    public void print(){
+
+        StackNode<T> temp = top;
+
+        while (true){
+
+            assert temp != null;
+            System.out.println("| "+temp.data+" |");
+
+            if (temp.next != null){
+
+                temp = temp.next;
+
+            }else {
+
+                break;
+
+            }
+
+
+
+        }
+        System.out.println("-----");
+
+    }
+
+
+
+}
+
+class StackNode<T>{
+
+    T data;
+    StackNode<T> next;
+
+    StackNode(T data){
+
+        this.data = data;
+        this.next = null;
+
+    }
+
+}
--- src/Stacks/Main.java
+++ src/Stacks/Main.java
@@ -1,11 +1,18 @@
 package Stacks;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
         ArrayStack<Integer> myStack = new ArrayStack<>(10);
 
+        LinkedListStack<Integer> listStack = new LinkedListStack<>();
+
         System.out.println("isEmpty -> "+myStack.isEmpty());
+        System.out.println("--------------------------------------------");
+        System.out.println("isEmpty -> "+listStack.isEmpty());
+        System.out.println();
 
         myStack.push(0);
         myStack.push(1);
@@ -18,11 +25,31 @@ public class Main {
         myStack.push(8);
         myStack.push(9);
 
+        listStack.push(0);
+        listStack.push(1);
+        listStack.push(2);
+        listStack.push(3);
+        listStack.push(4);
+        listStack.push(5);
+        listStack.push(6);
+        listStack.push(7);
+        listStack.push(8);
+        listStack.push(9);
+
         myStack.print();
+        System.out.println("\n--------------------------------------");
+        listStack.print();
+        System.out.println();
 
         System.out.println("\nisEmpty -> "+myStack.isEmpty());
 
         System.out.println("isFull -> " + myStack.isFull());
+
+        System.out.println("--------------------------------------------");
+
+        System.out.println("\nisEmpty -> "+listStack.isEmpty());
+
+
 
         System.out.println(myStack.top());
 
@@ -33,6 +60,16 @@ public class Main {
         System.out.println(myStack.topAndPop());
 
         myStack.print();
+
+        System.out.println("\n-----------------------------------------------");
+
+        System.out.println(listStack.peek());
+
+        listStack.print();
+
+        System.out.println(listStack.topAndPop());
+
+        listStack.print();
 
     }
 }
